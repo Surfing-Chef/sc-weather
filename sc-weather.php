97dc033c6ebb10bbcf13d2b404d620481a0c1fb9
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-05-26 15:58:10 -0700

    Function to create a Forecast cache file.

--- sc-weather.php
+++ sc-weather.php
@@ -19,3 +19,32 @@ require_once 'vendor/autoload.php';
 
 // Import Forecast namespace
 use Forecast\Forecast;
+
+// Create a forecast cache file.
+function forecast_cache ( $lat, $long, $token ){
+  /*
+   * $lat and $long are latitude and longitude respectively
+   * of location for forecast data retrieval
+   */
+
+   // Instantiate a new Forecast object
+   $forecast = new Forecast( $token );
+
+   // Get forecast object as json string
+   $options = json_encode( $forecast->get(
+     $lat,
+     $long,
+     null,
+     array(
+       'units' => 'si',
+       'exclude' => 'flags'
+     )
+   ));
+
+   // Store json string to file
+  $fp = fopen( 'forecast.json', 'w' );
+  fwrite( $fp, $options );
+  fclose( $fp );
+}
+
+forecast_cache ( '37.8267', '-122.423', $api );
