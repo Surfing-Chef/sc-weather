f9298a82b6082b40ee59b16bf7232a430890f5d7
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-05-28 12:56:22 -0700

    Function converts a bearing to a direction

--- sc-weather.php
+++ sc-weather.php
@@ -84,3 +84,29 @@ function output_cache($timeFrame, $key){
   return $timeFrame[$key];
 }
 
+// Convert bearing to direction
+function getDirection($bearing)
+{
+ $cardinalDirections = array(
+  'N' => array(337.5, 22.5),
+  'NE' => array(22.5, 67.5),
+  'E' => array(67.5, 112.5),
+  'SE' => array(112.5, 157.5),
+  'S' => array(157.5, 202.5),
+  'SW' => array(202.5, 247.5),
+  'W' => array(247.5, 292.5),
+  'NW' => array(292.5, 337.5)
+ );
+
+ foreach ($cardinalDirections as $dir => $angles)
+ {
+  if ($bearing >= $angles[0] && $bearing < $angles[1])
+  {
+   $direction = $dir;
+   break;
+  }
+ }
+ return $direction;
+}
+
+?>
